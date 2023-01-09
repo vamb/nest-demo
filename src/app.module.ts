6e12200f542c078c90427ea2985f02e01790aace
zhangsy <zhangsy@think-land.com>
2023-01-09 17:44:35 +0800

    feat: next is "Pipes" chapter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,26 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomerModule } from './customer/customer.module';
 import { CatModule } from "./cats/cat.module";
+import { LoggerMiddleware } from "./common/logger.middleware";
 
 @Module({
   imports: [CustomerModule, CatModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule{
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(LoggerMiddleware)
+      // .forRoutes({ path: 'cats', method: RequestMethod.GET });
+      // .forRoutes({ path: 'ab*cd', method: RequestMethod.ALL });
+      // .forRoutes(CatsController);
+      // .exclude(
+      //   { path: 'cats', method: RequestMethod.GET },
+      //   { path: 'cats', method: RequestMethod.POST },
+      //   'cats/(.*)',
+      // )
+      .forRoutes('cats')
+  }
+}
--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Post, HttpCode, Body, Query,
-    HttpStatus, Header, Param, Res, Req } from '@nestjs/common';
+    HttpStatus, Header, Param, Res, Req, HttpException,
+  ForbiddenException } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto'
 import { CatsService } from "./cats.service";
 import { CustomerService } from "../customer/customer.service";
@@ -10,6 +11,16 @@ export class CatsController {
       private readonly catsService: CatsService,
       private readonly customerService: CustomerService,
   ) {}
+
+    @Get('getException')
+    getException() {
+      throw new HttpException({
+        status: HttpStatus.FORBIDDEN,
+        error: 'This is a custom message',
+      }, HttpStatus.FORBIDDEN, {
+        // cause: 'sdsdfsdf'
+      })
+    }
 
     @Get('all')
     @HttpCode(HttpStatus.OK)
@@ -22,7 +33,16 @@ export class CatsController {
 
     @Get('findAll')
     findAll(@Res() response, @Req() request, @Query() query, @Body() body) {
-      response.status(HttpStatus.OK).send(
+      // response.status(HttpStatus.OK).send(
+      //   {
+      //     requestBody: request.body,
+      //     body: body,
+      //     requestQuery: request.query,
+      //     query: query,
+      //     data: [{name: 'test1'},{name: 'test2'},{name: 'test3'}]
+      //   }
+      // )
+      response.status(HttpStatus.OK).json(
         {
           requestBody: request.body,
           body: body,
